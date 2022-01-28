a7f66cad7d147857e97430bcf6adcfbe18588207
Yoshiaki Hara <>
2022-01-29 02:19:56 +0900

    update multipleSelector

--- components/organisms/multipleSelector.tsx
+++ components/organisms/multipleSelector.tsx
@@ -1,70 +1,107 @@
 import { ItemCategoryType } from '../../src/types/ItemCategoryType'
 import { ItemType } from '../../src/types/itemType'
-import { MenuItem, Select, SelectChangeEvent } from '@mui/material'
-import { useState } from 'react'
+import { InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material'
+import { useEffect, useState } from 'react'
 
 interface Props {
   itemCategoryList: ItemCategoryType[]
   itemList: ItemType[]
 }
 
-const MultipleSelector = ({ itemCategoryList, itemList }: Props) => {
-  const [category, setCategory] = useState<ItemCategoryType | null>(null)
-  const [item, setItem] = useState<ItemType | null>(null)
+const notSelectCategory: ItemCategoryType = {
+  id: -1,
+  name: '選択なし',
+  orderNo: -1,
+}
 
-  const selectItemByCategoryId = itemList.filter((i) => {
-    return i.categoryId === category?.id
+const notSelectItem: ItemType = {
+  id: -1,
+  name: '選択なし',
+  description: '',
+  imagePath: '',
+  categoryId: -1,
+}
+
+const MultipleSelector = ({ itemCategoryList, itemList }: Props) => {
+  const [categoryId, setCategoryId] = useState<number>(notSelectCategory.id)
+  const [itemId, setItemId] = useState<number>(notSelectItem.id)
+
+  const [category, setCategory] = useState<ItemCategoryType>(notSelectCategory)
+  const [item, setItem] = useState<ItemType>(notSelectItem)
+
+  const selectCategory = [notSelectCategory, ...itemCategoryList]
+  const selectItem = [notSelectItem, ...itemList]
+
+  const selectItemByCategoryId = selectItem.filter((i) => {
+    return i.categoryId === category?.id || i.id === notSelectItem.id
   })
 
-  const setCategoryById = (id: string) => {
+  const setCategoryById = (id: number) => {
     const category = itemCategoryList.filter((i) => {
-      return i.id === parseInt(id)
+      return i.id === id
     })
-    setCategory(category.length === 1 ? category[0] : null)
+    setCategory(category.length === 1 ? category[0] : notSelectCategory)
   }
 
-  const setItemById = (id: string) => {
+  const setItemById = (id: number) => {
     const item = itemList.filter((i) => {
-      return i.id === parseInt(id)
+      return i.id === id
     })
-    setItem(item.length === 1 ? item[0] : null)
+    setItem(item.length === 1 ? item[0] : notSelectItem)
   }
+
+  useEffect(() => {
+    setCategoryById(categoryId)
+    if (categoryId === notSelectCategory.id || itemId !== categoryId) {
+      setItemId(notSelectItem.id)
+    }
+  }, [categoryId])
+
+  useEffect(() => {
+    setItemById(itemId)
+  }, [itemId])
 
   return (
     <>
       <div>
         {/* category selector */}
+        <InputLabel id="category-selector-label">category</InputLabel>
         <Select
+          labelId="category-selector-label"
           label="category"
-          value={category?.id}
+          value={categoryId}
           placeholder="select item category"
-          style={{ minWidth: '10rem' }}
+          style={{
+            minWidth: '10rem',
+            color: `${categoryId !== notSelectCategory.id ? 'green' : 'gray'}`,
+          }}
+          variant="outlined"
           onChange={(e) => {
-            setCategoryById(e.target.value as string)
+            setCategoryId(e.target.value as number)
           }}
         >
-          <MenuItem key={-1} value="">
-            &nbsp;
-          </MenuItem>
-          {itemCategoryList.map((c, index) => (
+          {selectCategory.map((c, index) => (
             <MenuItem key={index} value={c.id}>
               {c.name}
             </MenuItem>
           ))}
         </Select>
         {/* item selector */}
+        <InputLabel id="item-selector-label">item</InputLabel>
         <Select
+          labelId="item-selector-label"
           label="item"
-          value={item?.id}
+          value={itemId}
           placeholder="select item"
-          style={{ minWidth: '10rem' }}
+          style={{
+            minWidth: '10rem',
+            color: `${itemId !== notSelectItem.id ? 'blue' : 'gray'}`,
+          }}
+          variant="outlined"
           onChange={(e) => {
-            setItemById(e.target.value as string)
+            setItemId(e.target.value as number)
           }}
         >
-          <MenuItem key={-1} value="">
-            &nbsp;
-          </MenuItem>
           {selectItemByCategoryId.map((i, index) => (
             <MenuItem key={index} value={i.id}>
               {i.name}
@@ -73,11 +110,24 @@ const MultipleSelector = ({ itemCategoryList, itemList }: Props) => {
         </Select>
       </div>
       {/* state display */}
-      <div>
-        category: {category?.id} {category?.name}
+      <div style={{ marginTop: '2rem' }}>
+        category:{' '}
+        <span
+          style={{
+            color: `${categoryId !== notSelectCategory.id ? 'green' : 'gray'}`,
+          }}
+        >
+          {category?.id} {category?.name}
+        </span>
       </div>
       <div>
-        item : {item?.id} {item?.name}
+        <span
+          style={{
+            color: `${itemId !== notSelectItem.id ? 'blue' : 'gray'}`,
+          }}
+        >
+          item : {item.id} {item.name}
+        </span>
       </div>
     </>
   )
